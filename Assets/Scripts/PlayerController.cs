7ae3a0d49d40d6b170ba517074cc4b4931572645
JCDM122300 <55261863+JCDM122300@users.noreply.github.com>
2020-03-09 22:16:13 -0500

    Created temporary levels and objects
    Created a test level with a skybox, temporary rising platform, and a tmporary player model. Also added a follow player script for the camera, a player controller script and a script for the platform to continuously rise.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float HorizontalInput;
+    public float MovementSpeed;
+    public float JumpForce;
+    public float GravityModifier;
+    public bool IsOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= GravityModifier;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        HorizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * HorizontalInput * Time.deltaTime * MovementSpeed);
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        {
+            playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            IsOnGround = false;
+        }
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsOnGround = true;
+    }
+}
---
+++ Assets/Scripts/RisingScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisingScript : MonoBehaviour
+{
+    
+    public float RisingSpeed;
+    public float TiltSpeed;
+
+    private Vector3 PlatformStartPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * RisingSpeed);
+    }
+
+
+}
